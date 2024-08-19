4cb1cbe3f89ec2ac8aaef9329962a69a01d17310
PalomaCarranza <164367090+PalomaCarranza@users.noreply.github.com>
2024-08-19 12:47:56 -0300

    Add files via upload

--- palomaAgostoUno.py
+++ palomaAgostoUno.py
@@ -1,27 +1,40 @@
 #Registro de notas
 #Ingresar la nota
 
-from calculadora import suma
+menu = '''
+  ###############################################
+  |     1 - Introducir nota                     |
+  |     2 - Mostrar promedio y cantidad de notas|
+  |     3 - Salir                               |
+  ############################################### '''
 
+print(menu)
+opcion = int(input("Ingrse la opcIón seleccionada: "))
 
-cantidadDeNotas=float(input("ingrese el número de notas que desea ingresar: "))
-for i in range(cantidadDeNotas):
-    nota= float(input("Ingrese la nota: "))
-#Almacenar en una lista
+from calculadora import sum
+
+if opcion == 1:
+    cantidadDeNotas=float(input("ingrese el número de notas que desea ingresar: "))
+    for i in range(cantidadDeNotas):
+        nota= float(input("Ingrese la nota: "))
+#Almacenar en una lista y verificar que la nota sea este entre 0 y 10
 notas=[]
 while True:
-    if nota>0 and nota<10:
+    if nota > 0 and nota < 11:
         notas.append(nota)
-    elif nota<0:
+    elif nota < 0 or nota > 11:
         print("Error: la nota no es válida")
-
-#Verificar que la nota sea este entre 0 y 10
-
-    
+        print(menu)
 #Calcular el promedio
 def promedio():
-    suma 
-    
+    promedio = sum(notas) / cantidadDeNotas
+    return promedio
+if opcion == 2:
+    promedio()
+    print (f"De {cantidadDeNotas} el promedio es {promedio}")
+
+if opcion == 3:
+     
 
 
 #Mostrar el promedio y si aprobo o no
